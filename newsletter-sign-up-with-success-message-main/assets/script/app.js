6f059fbaead5fc012e7ade7f41159c778adcc5d9
ESCADER, XYP COLLEN <116700665+Askelad777@users.noreply.github.com>
2023-09-01 04:54:47 +0800

    commit

--- newsletter-sign-up-with-success-message-main/assets/script/app.js
+++ newsletter-sign-up-with-success-message-main/assets/script/app.js
@@ -4,24 +4,71 @@ const btnSubscribe = document.getElementById('btn-subscribe');
 
 const successModal = document.getElementById('success-modal');
 
+const btnDsmMsg = document.getElementById('dismiss-button');
+
+const emailInput = document.getElementById('userEmail');
+
+const ErrorMessage = document.getElementById('Email-validation-message');
+
+
+
+// FUNCTION
 const toggleBackdrop = () =>{
   backdrop.classList.toggle('visible');
+}
+
+
+const backdropHandler = () =>{
+  backdrop.classList.remove('visible')
+  successModal.classList.remove('visible');
+
 };
 
 
-const successModalHandler = () =>{
+const showSuccessModal = () =>{  
   toggleBackdrop();
   successModal.classList.add('visible');
+};
+
+
+const closeSuccessModalHandler = ()=>{
+    backdropHandler();
 
 };
 
 
+// VALIDATION FIELD 
+
+const emailValidation = () =>{
+  if(!emailInput.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+
+    ErrorMessage.classList.add('visible');
+    ErrorMessage.innerHTML = 'Invalid Email';
+    return false;
+
+  }
+  ErrorMessage.innerHTML = 'Valid Email';
+  ErrorMessage.style.color = 'green';
+  showSuccessModal();
+  return true;
+};
 
 
+const showSuccessModalHandler = () => {
+  emailValidation();
+};
 
 
 
 
 // EVENTLISTENER
 
-btnSubscribe.addEventListener('click', successModalHandler);
+backdrop.addEventListener('click',backdropHandler);
+
+btnSubscribe.addEventListener('click', showSuccessModalHandler);
+
+
+btnDsmMsg.addEventListener('click', closeSuccessModalHandler)
+
+
+emailInput.addEventListener('input', emailValidation);
